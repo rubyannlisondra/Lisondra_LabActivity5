52581abb2df828678bfd2a69ec28537d16fbcb78
Ruby Ann <rubyannlisondra2@gmail.com>
2022-12-16 20:11:53 +0800

    Rename LeapYear Components

--- src/LeapYear/LeapYearGUI.java
+++ src/LeapYear/LeapYearGUI.java
@@ -1,9 +1,22 @@
 package LeapYear;
 
 import javax.swing.*;
+import java.awt.Color;
 
 public class LeapYearGUI {
     private JPanel panel1;
     private JTextField textField1;
     private JButton checkButton;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        JButton button = new JButton("Check");
+
+        frame.add(button);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400,400);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+
+    }
 }
