d03b95d90973040eaa0802974272cc6b93f3eca0
Ruby Ann <rubyannlisondra2@gmail.com>
2022-12-17 13:38:00 +0800

    Complete SimpleCalc

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -1,6 +1,8 @@
 package SimpleCalc;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class SimpleCalcGUI extends JFrame {
     private JPanel panel1;
@@ -9,6 +11,18 @@ public class SimpleCalcGUI extends JFrame {
     private JButton btnCompute;
     private JTextField lblResult;
     private JTextField tfNumber2;
+
+    public SimpleCalcGUI() {
+        btnCompute.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                calcComp();
+            }
+        });
+    }
+    public void calcComp() {
+
+    }
 
     public static void main(String[] args) {
         SimpleCalcGUI calc = new SimpleCalcGUI();
