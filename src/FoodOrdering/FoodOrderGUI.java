d675cd4aaed8312e80ed6f9f14ee64cff3c11fd8
Ruby Ann <rubyannlisondra2@gmail.com>
2022-12-17 14:02:47 +0800

    Add FoodOrdering Components

--- src/FoodOrdering/FoodOrderGUI.java
+++ src/FoodOrdering/FoodOrderGUI.java
@@ -2,11 +2,27 @@ package FoodOrdering;
 
 import javax.swing.*;
 
-public class FoodOrderGUI {
+public class FoodOrderGUI extends JFrame {
     private JPanel panel1;
-    private JCheckBox pizzaCheckBox;
+    private JCheckBox cPizza;
     private JRadioButton noneRadioButton;
-    private JButton orderButton;
+    private JButton btnOrder;
+    private JCheckBox cSoftDrinks;
+    private JRadioButton rb5;
+    private JRadioButton rb10;
+    private JRadioButton rb15;
+    private JCheckBox cBurger;
+    private JCheckBox cFries;
+    private JCheckBox cTea;
+    private JCheckBox cSundae;
 
+    public static void main(String[] args) {
+        FoodOrderGUI fOrder = new FoodOrderGUI();
 
+        fOrder.setContentPane(fOrder.panel1);
+        fOrder.setSize(600, 600);
+        fOrder.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        fOrder.setVisible(true);
+        fOrder.setTitle("Food Ordering System");
+    }
 }
