0cab5ee2b2de4a11b724da4791c78fb78181cff2
Ruby Ann <rubyannlisondra2@gmail.com>
2022-12-17 12:00:31 +0800

    Rename LeapYear Components

--- src/LeapYear/LeapYearGUI.java
+++ src/LeapYear/LeapYearGUI.java
@@ -1,22 +1,47 @@
 package LeapYear;
 
 import javax.swing.*;
-import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class LeapYearGUI {
     private JPanel panel1;
-    private JTextField textField1;
-    private JButton checkButton;
+    private JTextField tfYear;
+    private JButton btnCheckYear;
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame();
-        JButton button = new JButton("Check");
+        JFrame frame = new JFrame("Leap Year Checker");
+        JLabel label = new JLabel("Input Year");
+        label.setBounds(150, 100, 100, 100);
 
-        frame.add(button);
+        JTextField tfYear = new JTextField();
+        tfYear.setBounds(103, 41, 144, 20);
+        tfYear.setColumns(40);
+
+        JButton button = new JButton("Check");
+        button.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String input = JOptionPane.showInputDialog(null);
+                int yr = Integer.parseInt(input);
+                boolean leap = true;
+
+                if(leap) {
+                    JOptionPane.showMessageDialog(null, "Leap year");
+                }
+                else {
+                    JOptionPane.showMessageDialog(null, "Not a leap year");
+                }
+            }
+        });
+
+        frame.add(label);
+        frame.setLayout(null);
+        //frame.add(button);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(400,400);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+        frame.add(tfYear);
 
     }
 }
