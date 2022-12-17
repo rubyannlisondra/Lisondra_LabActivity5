36ff227930898f62c1b6ffde03f47603bfc85f97
Ruby Ann <rubyannlisondra2@gmail.com>
2022-12-17 14:06:02 +0800

    Add FoodOrdering main function

--- src/FoodOrdering/FoodOrderGUI.java
+++ src/FoodOrdering/FoodOrderGUI.java
@@ -1,6 +1,8 @@
 package FoodOrdering;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class FoodOrderGUI extends JFrame {
     private JPanel panel1;
@@ -15,6 +17,7 @@ public class FoodOrderGUI extends JFrame {
     private JCheckBox cFries;
     private JCheckBox cTea;
     private JCheckBox cSundae;
+
 
     public static void main(String[] args) {
         FoodOrderGUI fOrder = new FoodOrderGUI();
