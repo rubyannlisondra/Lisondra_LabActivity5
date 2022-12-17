5f1108bdafd87687e8634195e6236763f6c20a92
Ruby Ann <rubyannlisondra2@gmail.com>
2022-12-17 13:05:46 +0800

    Add exception handling to LeapYear

--- src/LeapYear/LeapYearGUI.java
+++ src/LeapYear/LeapYearGUI.java
@@ -4,11 +4,56 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class LeapYearGUI {
+public class LeapYearGUI extends JFrame {
     private JPanel panel1;
     private JTextField tfYear;
     private JButton btnCheckYear;
 
+    public LeapYearGUI() {
+        btnCheckYear.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                checkedYear();
+            }
+        });
+    }
+
+    public void checkedYear() {
+        try {
+            if(tfYear.getText() == " ") {
+                throw (new IllegalArgumentException("Empty String!"));
+            }
+            double yr = Double.parseDouble(tfYear.getText());
+            boolean leap = false;
+
+            if(yr < 0) {
+                throw (new IllegalArgumentException("Input is invalid!"));
+            }
+            if(yr % 4 == 0) {
+                if(yr % 100 == 0) {
+                    if(yr % 400 == 0) {
+                        leap = true;
+                    } else {
+                        leap = false;
+                    }
+                } else {
+                    leap = true;
+                }
+            } else {
+                leap = false;
+            }
+            if(leap) {
+                JOptionPane.showMessageDialog(panel1, "Leap year");
+            } else {
+                JOptionPane.showMessageDialog(panel1, "Not a Leap year");
+            }
+            tfYear.setText("");
+        }
+        catch(IllegalArgumentException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+            tfYear.setText("");
+        }
+    }
     public static void main(String[] args) {
         LeapYearGUI leapYear = new LeapYearGUI();
 
